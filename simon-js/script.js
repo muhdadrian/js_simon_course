a5e2de4f10027542a0afd5f758ca657f345b3f25
Adrian Nandu <adriannandu@gmail.com>
2025-12-01 21:12:29 +0800

    first commit

---
+++ simon-js/script.js
@@ -0,0 +1,13 @@
+'use strict';
+
+//alert('Good morning!');
+//alert('Adrian Nandu!');
+//console.log(10 + 5);
+//console.log(20 - 5);
+//console.log(2 + 2 - 5);
+// document.body.innerHTML = 'Good Morning!';
+// document.body.innerHTML = 'Adrian Nandu';
+
+// for (let i = 0, j = 10; i < j; i++, j--) {
+//   console.log(i + j);
+// }
